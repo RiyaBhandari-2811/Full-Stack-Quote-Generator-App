e9bbe3559e69f737262a7be892a6367869466c0f
Riya Bhandari <bhandari28riya@gmail.com>
2024-02-13 21:52:20 +0530

    Added the popup

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,13 @@ import Head from "next/head";
 import {
   FooterCon,
   FooterLink,
+  GenerateQuoteButton,
+  GenerateQuoteButtonText,
   GradientBgCom,
+  QuoteGeneratorCon,
+  QuoteGeneratorInnerCon,
+  QuoteGeneratorSubTitle,
+  QuoteGeneratorTitle,
 } from "@/components/QuoteGenerator/QuoteGeneratorElement";
 import { useState } from "react";
 
@@ -21,6 +27,34 @@ export default function Home() {
       </Head>
       {/* Background */}
       <GradientBgCom>
+        {/* Quote Generator Modal pop-up */}
+        {/* <QuoteGeneratorModal /> */}
+
+        {/* Quote Generator */}
+        <QuoteGeneratorCon>
+          <QuoteGeneratorInnerCon>
+            <QuoteGeneratorTitle>
+              Daily Inspiration Generator
+            </QuoteGeneratorTitle>
+            <QuoteGeneratorSubTitle>
+              Looking for a splash of inspiration? Generate a quote card with a
+              random inspirational quote provided by{" "}
+              <FooterLink
+                href="https://zenquotes.io/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                ZenQuotes API
+              </FooterLink>
+              .
+            </QuoteGeneratorSubTitle>
+            <GenerateQuoteButton>
+              <GenerateQuoteButtonText onClick={() => {}}>
+                Make a Quote
+              </GenerateQuoteButtonText>
+            </GenerateQuoteButton>
+          </QuoteGeneratorInnerCon>
+        </QuoteGeneratorCon>
         <FooterCon>
           <>
             Quotes Generated: {numberOfQuotes}
